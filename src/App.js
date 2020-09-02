6e9c1a2c68d615841a9db7f640fa7d07c63c1aa5
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-09-01 21:23:31 -0600

    add Formik form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Formik, Form, Field } from 'formik'
 
 function App() {
   return (
@@ -19,6 +20,30 @@ function App() {
           Learn React
         </a>
       </header>
+      <Formik 
+        initialValues={{ plant_name: '', plant_type: '' }}
+        validate={values => {
+          const errors = {}
+          if (!values.email){
+            errors.plantName = 'Required'
+          }
+          else if (!values.plantType){
+            errors.plantType = 'Required'
+          }
+          return errors;
+        }}
+        onSubmit = {(values) => {
+          alert(JSON.stringify(values, null, 2));
+          }
+        }>
+        {() => (
+          <Form>
+            <Field type="plant_name" name="plant_name" />
+            <Field type="plant_type" name="plant_type" />
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
